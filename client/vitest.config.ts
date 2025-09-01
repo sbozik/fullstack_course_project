b19748bb51c449a15b0d0a6e5bc67aa540fddfa2
sbozik <st.bozik87@gmail.com>
2025-09-01 11:44:30 +0200

    add edits for Ukol 5

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        environment: 'jsdom',
-        globals: true,
-        setupFiles: './vitest.setup.ts',
+  plugins: [react()],
+  test:
+    { environment: 'jsdom',
+      globals: true,
+      setupFiles: './vitest.setup.ts',
     },
 })
